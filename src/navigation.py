d12300a017e09f2c556b3dda065d0a08aec543b8
Florent Denat <denatflore@eisti.eu>
2021-07-22 17:01:11 +0200

    feat : find position

--- src/navigation.py
+++ src/navigation.py
@@ -4,68 +4,51 @@ import pytesseract
 from src.utils import *
 import numpy as np
 
+class Navigation:
+    def __init__(self):
+        self.vertical_position = 0
+        self.horizontal_position = 0
+
 # Move(direction)
 # MoveTo(position)
 # findPOS()
 def findPOS():
+    # Le pixel de l'écriture des POS vaut [228 228 226]
     imgGrab = ImageGrab.grab(bbox=(14,74,95,104))
-    # merged = Image.new("RGB", (1150, 115))
-    # number_box_start = [(585,395), (737, 395), (890, 395), (1040, 395), (1195, 395), (585,548), (737, 548), (890, 548), (1040, 548), (1195, 548)]
-        
-    # for i in range(10):
-    #     img = ImageGrab.grab(bbox=(number_box_start[i][0]+10,number_box_start[i][1]+10,number_box_start[i][0]+125,number_box_start[i][1]+125))
-    #     merged.paste(img, (int(i * 115),0))
     
     # img = np.array(merged)
-    def get_weight(non_zeros_values):
-            map_non_zeros_values_to_weight_values = {
-                0: 0,
-                3: 1,
-                5: 2,
-                255: 3
-            }
-            return map_non_zeros_values_to_weight_values[non_zeros_values]
+    # def get_weight(non_zeros_values):
+    #         map_non_zeros_values_to_weight_values = {
+    #             0: 0,
+    #             3: 1,
+    #             5: 2,
+    #             255: 3
+    #         }
+    #         return map_non_zeros_values_to_weight_values[non_zeros_values]
 
     img = np.array(imgGrab)
-    test = np.count_nonzero(img, axis=2)
-    v_get_weight = np.vectorize(get_weight)
-    test = np.array(list(map(v_get_weight, test)))
-    print(test)
-    # img = img[:, :, ::-1].copy()
-    # img[:,:,0] = np.zeros([img.shape[0], img.shape[1]])
-    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # cv2.imshow("Output In Grayscale", gray)
-    # cv2.waitKey(0)
-    # thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    # cv2.imshow("threshold", thresh)
-    # cv2.waitKey(0)
+    for lines in img:
+        for pixels in lines:
+            if pixels[0] < 220 or pixels[1] < 220 or pixels[2] < 220:
+                pixels[0] = 0
+                pixels[1] = 0
+                pixels[2] = 0
+            else:
+                pixels[0] = 255
+                pixels[1] = 255
+                pixels[2] = 255
+    # test = np.count_nonzero(img, axis=2)
+    # v_get_weight = np.vectorize(get_weight)
+    # test = np.array(list(map(v_get_weight, test)))
 
     # Connect text with a horizontal shaped kernel
-    # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10,3))
-    # cv2.imshow("kernel", kernel)
-    # cv2.waitKey(0)
-    # dilate = cv2.dilate(thresh, kernel, iterations=1)
-    # cv2.imshow("dilate", dilate)
-    # cv2.waitKey(0)
-    # PEUT ETRE ?
-    # closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
-
-    # Remove non-text contours using aspect ratio filtering
-    # cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    # for c in cnts:
-    #     x,y,w,h = cv2.boundingRect(c)
-    #     aspect = w/h
-    #     if aspect < 3:
-    #         cv2.drawContours(thresh, [c], -1, (0,0,0), -1)
-    # cv2.imshow("thresh2", thresh)
-    # cv2.waitKey(0)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2,1))
+    img = cv2.dilate(img, kernel, iterations=2)
 
     # Invert image and OCR
     result = 255 - img
-    # result = cv2.GaussianBlur(result,(5,5),cv2.BORDER_DEFAULT)
-    cv2.imshow("result", result)
-    cv2.waitKey(0)
-    cv2.imwrite("merged.png", result)
-    data = pytesseract.image_to_string(result, lang='eng',config='--psm 6 -c tessedit_char_whitelist=123456789-,')
+    # cv2.imshow("result", result)
+    # cv2.waitKey(0)
+    # cv2.imwrite("merged.png", result)
+    data = pytesseract.image_to_string(result, lang='eng',config='--psm 6 -c tessedit_char_whitelist=0123456789-,')
     return data.strip()
--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,13 @@
 import pyautogui
-import time
+from time import time, sleep
 from PIL import ImageGrab
+from threading import Lock, Thread
+import sys
 
 # printMousePosition()
 def printMousePosition():
     while True:
-        time.sleep(1)
+        sleep(1)
         print(pyautogui.position())
 
 # takeScreen(x1,y1,x2,y2)
@@ -21,7 +23,106 @@ def showScreen(screen):
 def altTab():
     pyautogui.keyDown('alt')
     pyautogui.keyDown('tab')
-    pyautogui.keyUp('alt')
     pyautogui.keyUp('tab')
+    pyautogui.keyUp('alt')
 
+#Décorateur chrono !
+def chrono(fonction):
+    def lancer(*args, **kwargs):
+        t_i = time()
+        resultat = fonction(*args, **kwargs)
+        t_f = time()
+        print("temps d'execution (en s):", t_f-t_i)
+        return resultat
+    return lancer
+
+def zero_pause_pyautogui_decorator(func):
+    def wrapper():
+        pyautogui.PAUSE = 0
+        func()
+        pyautogui.PAUSE = 0.1
+    return wrapper
+
+def FOCUS_DOFUS_SCREEN():
+    dofus_window = pyautogui.getWindowsWithTitle("Dofus")[0]
+    if not dofus_window.isActive:
+        if dofus_window.isMaximized:
+            dofus_window.minimize()
+        dofus_window.maximize()
+    sleep(1)
+
+def check_color(top_left_corner, bottom_right_corner, color):
+    # TODO: fix because it's doesn't work, it is fitted to red only
+    x1, y1 = top_left_corner
+    x2, y2 = bottom_right_corner
+    red, green, blue = color
+    
+    img = ImageGrab.grab(bbox=(x1, y1, x2, y2))
+    pix = img.load()
+    
+    for pix_x in range(img.size[0]):
+        for pix_y in range(img.size[1]):
+            r, g, b = pix[(pix_x, pix_y)]
+            if r > red and g < green and b < blue:
+                return True
+    return False
+
+def check_pixel_color(pixel_position, color):
+    x, y = pixel_position
+    im = pyautogui.screenshot(region=(x, y, 1, 1))
+    return im.getpixel((0,0)) == color
+
+def check_image(image_path, region, grayscale=True, confidence=.65):
+    coordinates = pyautogui.locateOnScreen(image_path, grayscale=grayscale, confidence=confidence, region=region)
+    return coordinates is not None
+
+class KillableThread(Thread):
+    def __init__(self, *args, **keywords):
+        Thread.__init__(self, *args, **keywords)
+        self.killed = False
+    
+    def start(self):
+        self.__run_backup = self.run
+        self.run = self.__run      
+        Thread.start(self)
+    
+    def __run(self):
+        sys.settrace(self.globaltrace)
+        self.__run_backup()
+        self.run = self.__run_backup
+    
+    def globaltrace(self, frame, event, arg):
+        if event == 'call':
+            return self.localtrace
+        else:
+            return None
+    
+    def localtrace(self, frame, event, arg):
+        if self.killed:
+            if event == 'line':
+                raise SystemExit()
+        return self.localtrace
+    
+    def kill(self):
+        self.killed = True
+
+class SingletonMeta(type):
+    """
+    This is a thread-safe implementation of Singleton.
+    Source: https://refactoring.guru/fr/design-patterns/singleton/python/example#example-0
+    """
+
+    _instances = {}
+    _lock: Lock = Lock()
+
+    def __call__(cls, *args, **kwargs):
+        """
+        Possible changes to the value of the `__init__` argument do not affect
+        the returned instance.
+        """
+        with cls._lock:
+            if cls not in cls._instances:
+                instance = super().__call__(*args, **kwargs)
+                cls._instances[cls] = instance
+        return cls._instances[cls]
 # checkPods()
